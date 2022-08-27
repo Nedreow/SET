019223072c8b78cde2207cf8ef6e651a58325b51
Nedreow <F.vanwoerden23@gmail.com>
2022-08-27 16:46:28 +0200

    Add symbols to the card

--- Card.cs
+++ Card.cs
@@ -1,34 +1,40 @@
 using Godot;
 using System;
+using SET;
 
 public class Card : Node2D
 {
-    // Declare member variables here. Examples:
-    [Export]
     public int CardSize = 250;
 
-    public int SymbolShape;
-    public int SymbolCount;
-    public int SymbolColor;
-    public int SymbolFill;
+    [Export] public float cardScale = 1f;
+
+    public readonly int SymbolShape;
+    public readonly int SymbolCount;
+    public readonly int SymbolColor;
+    public readonly int SymbolFill;
 
     public Card()
     {
-        SymbolShape = Convert.ToInt32(GD.Randi() % 3);
-        SymbolCount = Convert.ToInt32(GD.Randi() % 3);
-        SymbolColor = Convert.ToInt32(GD.Randi() % 3);
-        SymbolFill = Convert.ToInt32(GD.Randi() % 3);
+        SymbolShape = 2;
+        SymbolCount = 2;
+        SymbolColor = 3;
+        SymbolFill = 3;
     }
 
     // Called when the node enters the scene tree for the first time.
     public override void _Ready()
     {
-        
+        base._Ready();
+
+        for (int i = 0; i < SymbolCount; i++)
+        {
+            addSymbol(i);
+        }
     }
     
     public override void _Draw()
     {
-        var cardDimensions = new Rect2(0, 0, CardSize, CardSize * 1.4f);
+        var cardDimensions = new Rect2(0, 0, CardSize * cardScale, CardSize * 1.4f * cardScale);
         var cardColor = new Color(1, 1, 0.941176F, 1);
         
         var styleBox = new StyleBoxFlat();
@@ -36,6 +42,28 @@ public class Card : Node2D
         styleBox.BgColor = cardColor;
         
         DrawStyleBox(styleBox, cardDimensions);
+    }
+
+    private void addSymbol(int location)
+    {
+        float startingOffset = 0f;
+        switch (SymbolCount)
+        {
+            case 1:
+                startingOffset = 1.9f;
+                break;
+            case 2:
+                startingOffset = 0.95f;
+                break;
+        }
+        
+        var sprite = new Sprite();
+        sprite.Texture = Config.Textures[SymbolShape][SymbolFill];
+        sprite.Scale = new Vector2(0.20f * cardScale, 0.20f * cardScale);
+        sprite.Offset = new Vector2(CardSize * 2.5f, CardSize * (1.6f + startingOffset + 1.9f * location));
+        sprite.Modulate = Config.Colors[SymbolColor];
+        
+        AddChild(sprite);
     }
 
 //  // Called every frame. 'delta' is the elapsed time since the previous frame.
---
+++ Config.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections.Generic;
+using System.Diagnostics;
+using Godot;
+
+namespace SET
+{
+    public class Config
+    {
+        private static Dictionary<int, string> _shapes = new Dictionary<int, string>()
+        {
+            { 1, "Oval" },
+            { 2, "Rectangle" },
+            { 3, "Wave" },
+        };
+
+        private static Dictionary<int, string> _fills = new Dictionary<int, string>()
+        {
+            { 1, "Outline" },
+            { 2, "Barred" },
+            { 3, "Solid" },
+        };
+
+        public static Dictionary<int, Dictionary<int, Texture>> Textures =
+            new Dictionary<int, Dictionary<int, Texture>>()
+            {
+                {
+                    1, new Dictionary<int, Texture>()
+                    {
+                        { 1, ResourceLoader.Load<Texture>(_resourcePath(1, 1)) },
+                        { 2, ResourceLoader.Load<Texture>(_resourcePath(1, 2)) },
+                        { 3, ResourceLoader.Load<Texture>(_resourcePath(1, 3)) }
+                    }
+                },{
+                    2, new Dictionary<int, Texture>()
+                    {
+                        { 1, ResourceLoader.Load<Texture>(_resourcePath(2, 1)) },
+                        { 2, ResourceLoader.Load<Texture>(_resourcePath(2, 2)) },
+                        { 3, ResourceLoader.Load<Texture>(_resourcePath(2, 3)) }
+                    }
+                },{
+                    3, new Dictionary<int, Texture>()
+                    {
+                        { 1, ResourceLoader.Load<Texture>(_resourcePath(3, 1)) },
+                        { 2, ResourceLoader.Load<Texture>(_resourcePath(3, 2)) },
+                        { 3, ResourceLoader.Load<Texture>(_resourcePath(3, 3)) }
+                    }
+                }
+            };
+
+        public static Dictionary<int, Color> Colors = new Dictionary<int, Color>()
+        {
+            { 1, Color.Color8(197, 12, 12) },
+            { 2, Color.Color8(0, 127, 0) },
+            { 3, Color.Color8(73, 0, 129) },
+        };
+        
+        private static string _resourcePath(int shape, int fill) 
+        {
+            return $"Resources/{_shapes[shape]} {_fills[fill]}.png";
+        }
+    };
+
+        
+}
